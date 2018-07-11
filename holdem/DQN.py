4821dde1fd7cb7a6faa312b093bd404c3d729af7
Kuo Yuan Chiang <kuo_yuan_chiang@ids-b-ky002.iad1>
2018-07-11 12:35:20 +0200

    1. remove current_player from observation

--- holdem/DQN.py
+++ holdem/DQN.py
@@ -83,7 +83,7 @@ class DeepQNetwork:
         with tf.variable_scope('loss'+ self.nickname, reuse=tf.AUTO_REUSE):
             self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
         with tf.variable_scope('train'+ self.nickname, reuse=tf.AUTO_REUSE):
-            self._train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
         # ------------------ build target_net ------------------
         self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')    # input
